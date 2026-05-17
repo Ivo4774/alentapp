ced624ab9b1fe2f5802c9422a90d28c258471329
belenpieroni <mbpieroni18@gmail.com>
2026-05-17 17:44:31 -0300

    feat(payment): modifico payment repository para que acepte filtros

--- packages/api/src/domain/PaymentRepository.ts
+++ packages/api/src/domain/PaymentRepository.ts
@@ -1,10 +1,10 @@
-import { CreatePaymentRequest, PaymentDTO, PaymentStatus } from '@alentapp/shared';
+import { CreatePaymentRequest, PaymentDTO, PaymentStatus, GetPaymentsQuery } from '@alentapp/shared';
 
 // Esta interfaz es el "Puerto de Salida" para los Pagos.
 export interface PaymentRepository {
   create(data: CreatePaymentRequest): Promise<PaymentDTO>;
   findById(id: string): Promise<PaymentDTO | null>;
-  findAll(): Promise<PaymentDTO[]>;
+  findAll(filters?: GetPaymentsQuery): Promise<PaymentDTO[]>;
   findByMemberId(memberId: string): Promise<PaymentDTO[]>;
   updateStatus(id: string, status: PaymentStatus, paymentDate: Date): Promise<PaymentDTO>;
   cancel(id: string): Promise<PaymentDTO>;
